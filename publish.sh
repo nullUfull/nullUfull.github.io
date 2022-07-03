98b412d1cf05351c4fef94a5b78d89dbeb7b3517
alkaidwang <alkaidwang@tencent.com>
2022-07-04 02:03:02 +0800

    update

--- publish.sh
+++ publish.sh
@@ -18,7 +18,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo -F --cleanDestinationDir 
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
